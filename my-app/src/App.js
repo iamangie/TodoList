19050db3151c364380de966cb22754c070c9b7f2
iamangie <angie.diachuk@gmail.com>
2020-01-25 19:36:54 +0200

    new todo

---
+++ my-app/src/App.js
@@ -0,0 +1,73 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const User = ({ id, name, deleteUser, editUser }) => {
+ const [userName, setUserName] = useState(name);
+  const editUserName = ({keyCode}) => {
+    console.log('keypress', keyCode)
+    if(keyCode === 13) {
+      console.log('edit user name !')
+      editUser(id, userName);
+    }
+  }
+   return (
+    <div>
+        <input onKeyDown={(e) => editUserName(e)} value={userName} onChange={(e) =>  setUserName(e.target.value) } />
+          <div>{id}</div>
+          <div onClick={() => { deleteUser(id) }}>delete</div>
+    </div>
+  );
+};
+
+
+function App() {
+
+  
+  
+  const [users, setUsers] = useState([
+    {name: 'Valera', id: 1,},
+    {name: 'Kolya', id: 2},
+  ]);
+
+  const [value, setValue] = useState('hui');
+
+  const addUser = ({keyCode}) => {
+    if(keyCode === 13) {
+      setValue('');
+      setUsers([...users, {name: value, id: users.length + 1}]);
+    }
+  }
+
+  const deleteUser = (id) => {
+    const filteredUsers = users.filter(u => u.id !== id);
+    setUsers(filteredUsers);
+  }
+
+  const editUser = (id, name) => {
+    const filteredUsers = users.filter(u => u.id !== id);
+    const editedUser = users.filter(u => u.id == id);
+    editedUser[0].name = name;
+    setUsers([...filteredUsers, ...editedUser]);
+  }
+
+
+  return (
+    <div className="App">
+      <input 
+      onKeyDown={(e) => {addUser(e)}} 
+      onChange={(e) => {setValue(e.target.value)}} 
+      value={value}
+      />
+    {
+      users.map(u => {
+        return (
+          <User key={u.id} id={u.id} name={u.name} deleteUser={deleteUser} editUser={editUser}/>
+        )
+      })
+    }
+    </div>
+  );
+}
+
+export default App;
